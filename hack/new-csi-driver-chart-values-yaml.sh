b507ad639c522bad9edd6ce94ccf51ba0858e5d7
stoneshi-yunify <stoneshi@kubesphere.io>
2022-03-23 09:09:11 +0800

    update new driver script
    Signed-off-by: stoneshi-yunify <stoneshi@kubesphere.io>

--- hack/new-csi-driver-chart-values-yaml.sh
+++ hack/new-csi-driver-chart-values-yaml.sh
@@ -26,30 +26,52 @@ driver:
 
 snapshot-controller:
   enabled: false
+  repository: csiplugin/snapshot-controller
+  tag: v2.0.1
 
 attacher:
   clusterRoleName: external-attacher-runner${suffix}
   clusterRoleBindingName: csi-attacher-role${suffix}
+  repository: csiplugin/csi-attacher
+  tag: v3.2.1
 
 plugins:
+  healthMonitorAgent:
+    repository: csistorage/csi-external-health-monitor-agent
+    tag: v0.2.0
+  nodeDriverRegistrar:
+    repository: csistorage/k8scsi-csi-node-driver-registrar
+    tag: v2.1.0
+  livenessProbe:
+    repository: csistorage/k8scsi-livenessprobe
+    tag: v2.2.0
   healthMonitorController:
     clusterRoleName: external-health-monitor-controller-runner${suffix}
     clusterRoleBindingName: csi-health-monitor-controller-role${suffix}
+    repository: csistorage/csi-external-health-monitor-controller
+    tag: v0.2.0
   hostPathPlugin:
+    repository: csistorage/hostpathplugin
     tag: v0.9.0
     dataDir: /var/lib/csi-hostpath-data${suffix}/
 
 provisioner:
   clusterRoleName: external-provisioner-runner${suffix}
   clusterRoleBindingName: csi-provisioner-role${suffix}
+  repository: csiplugin/csi-provisioner
+  tag: v2.2.2
 
 resizer:
   clusterRoleName: external-resizer-runner${suffix}
   clusterRoleBindingName: csi-resizer-role${suffix}
+  repository: csiplugin/csi-resizer
+  tag: v1.2.0
 
 snapshotter:
   clusterRoleName: external-snapshotter-runner${suffix}
   clusterRoleBindingName: csi-snapshotter-role${suffix}
+  repository: csiplugin/csi-resizer
+  tag: v4.0.0
 
 " > $out
 
